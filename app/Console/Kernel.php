601a3864b02bc43fd9bee70f7bcae3c141206697
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-05-03 05:36:05 +0000

    Update the kernel to process recalled products

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\SyncAllRecalledProducts;
 use App\Jobs\SyncMichiganRecallsJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -11,6 +12,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(SyncMichiganRecallsJob::class)->dailyAt('08:00');
+        $schedule->job(SyncAllRecalledProducts::class)->dailyAt('09:00');
     }
 
     protected function commands()
